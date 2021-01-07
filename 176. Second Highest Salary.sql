48f692e8991b16ef8815971ab52728b5017b7950
twyunting <yc6705a@american.edu>
2021-01-07 15:19:57 -0500

    Update 176. Second Highest Salary.sql

--- 176. Second Highest Salary.sql
+++ 176. Second Highest Salary.sql
@@ -1,7 +1,12 @@
 # Write your MySQL query statement below
 
+# solution 1
+SELECT max(Salary) AS SecondHighestSalary FROM Employee
+WHERE Salary < (SELECT max(Salary) FROM Employee);
+
+
+# solution 2
 SELECT(SELECT DISTINCT
     Salary FROM Employee
     ORDER BY Salary DESC
     LIMIT 1 OFFSET 1) AS SecondHighestSalary
-# first sql question
