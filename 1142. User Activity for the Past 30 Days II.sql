e9e3c98394208c2f2d6ba28cce405aaee063a4cb
twyunting <yc6705a@american.edu>
2021-05-15 15:34:32 -0400

    Create 1142. User Activity for the Past 30 Days II.sql

---
+++ 1142. User Activity for the Past 30 Days II.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+
+SELECT IFNULL(ROUND(COUNT(DISTINCT session_id)/COUNT(DISTINCT user_id), 2), 0)
+AS average_sessions_per_user
+FROM Activity
+WHERE activity_date BETWEEN "2019-06-28" AND "2019-07-27";
