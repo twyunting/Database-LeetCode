61c8c313e909edf80b4136d9201add35055b5248
twyunting <yc6705a@american.edu>
2021-06-26 16:27:24 -0400

    Update 534. Game Play Analysis III.sql

--- 534. Game Play Analysis III.sql
+++ 534. Game Play Analysis III.sql
@@ -6,3 +6,10 @@ from Activity A
 inner join Activity B on A.player_id = B.player_id
 and A.event_date >= B.event_date 
 group by A.player_id, A.event_date;
+
+# solution 2
+SELECT
+player_id, event_date, sum(games_played) over(PARTITION BY player_id ORDER BY event_date)
+AS 'games_played_so_far'
+FROM activity
+ORDER BY player_id, games_played_so_far;
