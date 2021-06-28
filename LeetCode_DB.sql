7219a8db046e1961db37c4fd523ad298efe56226
twyunting <yc6705a@american.edu>
2021-06-28 15:11:39 -0400

    6/28

--- LeetCode_DB.sql
+++ LeetCode_DB.sql
@@ -1,2 +1,22 @@
 create database if not exists LeetCode;
 use LeetCode;
+Create table If Not Exists Activity (player_id int, device_id int, event_date date, games_played int);
+Truncate table Activity;
+insert into Activity (player_id, device_id, event_date, games_played) values ('1', '2', '2016-03-01', '5');
+insert into Activity (player_id, device_id, event_date, games_played) values ('1', '2', '2016-05-02', '6');
+insert into Activity (player_id, device_id, event_date, games_played) values ('1', '3', '2017-06-25', '1');
+insert into Activity (player_id, device_id, event_date, games_played) values ('3', '1', '2016-03-02', '0');
+insert into Activity (player_id, device_id, event_date, games_played) values ('3', '4', '2018-07-03', '5');
+
+select * from Activity;
+select * 
+from Activity A
+inner join Activity B on A.player_id = B.player_id
+and A.event_date >= B.event_date;
+
+Create table If Not Exists Products (product_id int, store1 int, store2 int, store3 int);
+Truncate table Products;
+insert into Products (product_id, store1, store2, store3) values ('0', '95', '100', '105');
+insert into Products (product_id, store1, store2, store3) values ('1', '70', 'None', '80');
+
+select * from Products
