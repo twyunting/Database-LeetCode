0eba4c5d19f68a6e33d724fe183ba385a1d822fd
twyunting <yc6705a@american.edu>
2021-07-16 12:03:17 -0400

    Create 580. Count Student Number in Departments.sql

---
+++ 580. Count Student Number in Departments.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+select d.dept_name, count(student_name) as student_number from student s
+right join department d
+on s.dept_id = d.dept_id
+group by d.dept_name
+order by student_number desc, d.dept_name;
